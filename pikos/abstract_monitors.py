6676f5322395a81c4fb308437399ac5d026130b4
itziakos <ioannist@enthought.com>
2012-04-06 18:10:53 +0100

    Rework abstract interfaces (naming, methods and docstring) the AbstractTimeMonitor needs a little more work.

--- pikos/abstract_monitors.py
+++ pikos/abstract_monitors.py
@@ -1,83 +1,63 @@
 import abc
 import sys
 
-class PikosError(Exception):
-    """ Base pikos error.
-    """
-    pass
-
 class AbstractMonitor(object):
-    """ The abstract monitor class.
+    """ The base abstract monitor class.
 
-    This is an abstract class for all the profiles.
-
+    This is an abstract class for all monitors and  profilers.
 
     """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, item):
+        """ Class initialization.
+
+        Parameters
+        ----------
+        item : object
+            The item to monitor.
+
+        """
+        self._item = item
+
     def run(self):
-        """Run the code under examination.
+        """ Start the monitoring.
         """
         self.setup()
         try:
-            result = self.process()
-        except Exception as error:
-            msg = "This error occured while running the provided code: {0}".\
-                    format(error)
-            PikosError(msg)
+            result = self.monitor_item()
         finally:
             self.teardown()
         return result
 
     @abc.abtsractmethod
     def setup(self):
-        """Setup the monitor.
+        """ Setup the monitor.
         """
         pass
 
     @abc.abtsractmethod
     def teardown(self):
-        """Tear down the monitor.
+        """ Tear-down the monitor.
         """
         pass
 
     @abc.abstractmethod
-    def process(self):
-        """process to executed during monitoring.
+    def monitor_item(self):
+        """ Method executes the item under examination.
+
+        Anything that the examined item returns should be forwarded
+        and returned by this function to the caller.
+
         """
         pass
 
 
-class AbstractTimeMonitor(AbstractMonitor):
-    """Abstract monitor over fixed time itervals.
-    """
-    def __init__(self, interval=250):
-        """Class Initialisation.
-        """
-        self.interval = interval
-
-    def process(self):
-        while self.reset_timer():
-            self.on_time_event()
-            time.sleep(self.interval)
-
-    @abc.abstractmethod
-    def reset_timer(self):
-        """ check if we need to reset the timer and continue monitoring.
-        """
-        return False
-
-    @abc.abstractmethod
-    def on_time_event(self):
-        """Line event despatcher.
-        """
-        pass
-
-
-class AbstractFunctionMonitor(AbstractProfiler):
-    """Abstract monitor for function events using the setprofile interface.
+class AbstractFunctionMonitor(AbstractMonitor):
+    """ Abstract monitor on function events using the setprofile interface.
     """
     def __init__(self, callable):
-        """Class Initialisation.
-        """
         self.process = callable
 
     def setup(self):
@@ -88,13 +68,13 @@ class AbstractFunctionMonitor(AbstractProfiler):
 
     @abc.abstractmethod
     def on_function_event(self, frame, event, arg):
-        """Function event despatcher.
+        """ Function event dispatcher.
         """
         pass
 
 
 class AbstractLineMonitor(AbstractMonitor):
-    """Abstract monitor using the settrace interface.
+    """ Abstract monitor using the settrace interface.
     """
     def __init__(self, callable):
         """ Class Initialisation.
@@ -109,6 +89,25 @@ class AbstractLineMonitor(AbstractMonitor):
 
     @abc.abstractmethod
     def on_line_event(self, frame, event, arg):
+        """ Line event despatcher.
+        """
+        pass
+
+
+class AbstractTimeMonitor(AbstractMonitor):
+    """ Abstract monitor to examine an external process over fixed
+    time itervals.
+
+    A TimeMonitor should setup a timer in the :meth:`setup` method
+    that will call the :meth:`on_time_event` to record the necessary
+    information.
+
+    """
+    def __init__(self, interval):
+        self.interval = interval
+
+    @abc.abstractmethod
+    def on_time_event(self):
         """Line event despatcher.
         """
         pass
