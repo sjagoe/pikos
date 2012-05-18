669e00fcc18ce5365c682037bb6e86ddfe45205f
itziakos <ioannist@enthought.com>
2012-05-18 18:58:16 +0100

    Add function memory logger

---
+++ pikos/loggers/function_memory_logger.py
@@ -0,0 +1,75 @@
+from __future__ import absolute_import
+import inspect
+import os
+import psutil
+from collections import namedtuple
+from pikos._internal.profile_functions import ProfileFunctions
+from pikos.recorders.abstract_record_formater import AbstractRecordFormater
+
+__all__ = [
+    'FunctionMemoryLogger',
+    'FunctionMemoryRecord',
+    'FunctionMemoryRecordFormater'
+]
+
+FUNCTION_MEMORY_RECORD = ('index', 'type', 'function', 'RSS', 'VMS', 'lineNo',
+                          'filename')
+FUNCTION_MEMORY_RECORD_TEMPLATE = ('{:<8} {:<11} {:<8} {:<8} {:<30} {:<5} {}'
+                                   '{newline}')
+
+FunctionMemoryRecord = namedtuple('FunctionMemoryRecord',
+                                  FUNCTION_MEMORY_RECORD)
+
+# we might need a factory function for this
+class FunctionMemoryRecordFormater(AbstractRecordFormater):
+
+    def header(self, record):
+        return FUNCTION_MEMORY_RECORD_TEMPLATE.format(*record._fields,
+                                                      newline=os.linesep)
+
+    def line(self, record):
+        return FUNCTION_MEMORY_RECORD_TEMPLATE.format(*record,
+                                                      newline=os.linesep)
+
+class FunctionMemoryLogger(object):
+    """ Log process memory on python function events. """
+
+    _fields = FunctionMemoryRecord._fields
+
+    def __init__(self, recorder):
+        """ Initialize the function memory profiler class.
+
+        Parameters
+        ----------
+        recorder : pikos.recorders.AbstractRecorder
+            An instance of a Pikos recorder to handle the values to be logged
+        """
+        self._recorder = recorder
+        self._profiler = ProfileFunctions()
+        self._index = 0
+        self._run_counts = 0
+        self._process = None
+
+    def __enter__(self):
+        self._run_counts += 1
+        if self._run_counts == 1:
+            self._process = psutil.Process(os.getpid())
+            self._recorder.prepare(self._fields)
+            self._profiler.set(self.on_function_event)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._run_counts -= 1
+        if self._run_counts == 0:
+            self._profiler.unset()
+            self._recorder.finalize()
+
+    def on_function_event(self, frame, event, arg):
+        usage = self._process.get_memory_info()
+        filename, lineno, function, _, _ = \
+            inspect.getframeinfo(frame, context=0)
+        if event.startswith('c_'):
+            function = arg.__name__
+        record = FunctionMemoryRecord(self._index, event, function, usage.rss,
+                                      usage.vms, filename, lineno)
+        self._recorder.record(record)
+        self._index += 1
---
+++ pikos/tests/test_function_memory_logger.py
@@ -0,0 +1,93 @@
+import StringIO
+import unittest
+
+from pikos.monitor import monitor
+from pikos.loggers.function_memory_logger import FunctionMemoryLogger
+from pikos.recorders.list_recorder import ListRecorder
+from pikos.tests.test_assistant import TestAssistant
+
+class TestFunctionMemoryLogger(unittest.TestCase, TestAssistant):
+
+    def test_function(self):
+        recorder = ListRecorder()
+        logger = FunctionMemoryLogger(recorder)
+
+        @monitor(logger)
+        def gcd(x,y):
+            while x > 0:
+                x, y = y % x, x
+            return y
+
+        def boo():
+            pass
+
+        boo()
+        result = gcd(12, 3)
+        boo()
+        self.assertEqual(result, 3)
+        records = recorder.records
+        # check that the records make sense
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('gcd', 'call'), times=1)
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('gcd', 'return'), times=1)
+        self.assertFieldValueNotExist(records, ('function',), ('boo',))
+        # The wrapper of the function should not be logged
+        self.assertFieldValueNotExist(records, ('function',), ('wrapper',))
+
+    def test_recursive(self):
+        recorder = ListRecorder()
+        logger = FunctionMemoryLogger(recorder)
+
+        @monitor(logger)
+        def gcd(x,y):
+            return x if y == 0 else gcd(y, (x % y))
+
+        def boo():
+            return gcd(7,12)
+
+        result = boo()
+        self.assertEqual(result, 1)
+        records = recorder.records
+        # check that the records make sense
+        # The function should be called 6 times
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('gcd', 'call'), times=6)
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('gcd', 'return'), times=6)
+        self.assertFieldValueNotExist(records, ('function',), ('boo',))
+        # In recursive calls the wrapper of the function is logged
+        # self.assertFieldValueNotExist(records, ('function',), ('wrapper',))
+
+    def test_generator(self):
+        recorder = ListRecorder()
+        logger = FunctionMemoryLogger(recorder)
+        output = (0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
+
+        @monitor(logger)
+        def fibonacci(items):
+            x, y = 0, 1
+            for i in range(items):
+                yield x
+                x, y = y, x + y
+
+        def boo():
+            pass
+
+        boo()
+        result = [value for value in fibonacci(10)]
+        boo()
+        self.assertSequenceEqual(result, output)
+        records = recorder.records
+        # check that the records make sense
+        # The function should be called 10 + 1 times
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('fibonacci', 'call'), times=11)
+        self.assertFieldValueExist(records, ('function', 'type'),
+                                   ('fibonacci', 'return'), times=11)
+        self.assertFieldValueNotExist(records, ('function',), ('boo',))
+        # The wrapper of the generator should not be logged
+        self.assertFieldValueNotExist(records, ('function',), ('wrapper',))
+
+if __name__ == '__main__':
+    unittest.main()
