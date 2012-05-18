f6f16d006d2f61385b1ba1aca3b5d185d225ecb5
itziakos <ioannist@enthought.com>
2012-05-18 19:09:11 +0100

    add a function to trim to a specific length strings by removing characters from left side.

--- pikos/_internal/util.py
+++ pikos/_internal/util.py
@@ -4,3 +4,9 @@ def is_context_manager(obj):
     """ Check if the obj is a context manager """
     # FIXME: this should work for now.
     return hasattr(obj, '__enter__') and hasattr(obj, '__exit__')
+
+def trim_left(value, max_length):
+    """ Trim the left side of the string so that the lenght is at most max_length.
+    """
+    str_value = str(value)
+    return str_value[-max_length:len(str_value)]
