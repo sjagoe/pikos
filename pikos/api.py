4c5075cb9dc8295b410ff15599f1ab59c45740b8
itziakos <ioannist@enthought.com>
2012-05-30 18:15:12 +0100

    updated apy fuctory functions to accept record filters

--- pikos/api.py
+++ pikos/api.py
@@ -2,17 +2,17 @@ import sys
 
 from pikos.monitor import Monitor
 from pikos.recorders.text_stream_recorder import TextStreamRecorder
-from pikos.loggers.function_logger import FunctionLogger
+from pikos.monitors.function_monitor import FunctionMonitor
 
-def baserecorder():
+def baserecorder(filter_):
     """ Factory function that returns a default recorder.
     """
-    return TextStreamRecorder(sys.stdout, auto_flush=True)
+    return TextStreamRecorder(sys.stdout, filter_=filter_, auto_flush=True)
 
-def log_functions():
+def log_functions(filter_):
     """ Factory function that returns a default function logger.
     """
-    return FunctionLogger(baserecorder())
+    return FunctionMonitor(baserecorder(filter_=filter_))
 
 #: Easy to find placeholder for the Monitor decorator class.
 monitor = Monitor
