dd4b04c2e47f136c8e912534a0980c27652eb9c2
itziakos <ioannist@enthought.com>
2012-05-31 19:26:19 +0100

    use the KeepTrack in the context manager

--- pikos/external/python_cprofiler.py
+++ pikos/external/python_cprofiler.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 import sys
 import cProfile
-import profile
 
+from pikos._internal.keep_track import KeepTrack
 
 __all__ = [
     'PythonCProfiler',
@@ -10,18 +10,18 @@ __all__ = [
 
 class PythonCProfiler(cProfile.Profile):
     """ The normal python cProfiler adapted to work with the pikos Monitor class.
+
+
     """
     def __init__(self, *args, **kwrds):
         super(PythonCProfiler, self).__init__(*args, **kwrds)
-        self._run_count = 0
+        self._call_tracker = KeepTrack()
 
     def __enter__(self):
-        self._run_count += 1
-        if self._run_count == 1:
+        if self._call_tracker('ping'):
             self.enable()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self._run_count -= 1
-        if self._run_count == 0:
+        if self._call_tracker('pong'):
             self.disable()
 
