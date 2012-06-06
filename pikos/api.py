eedb250b8ce1af738863c13f839ce6529679a23c
itziakos <ioannist@enthought.com>
2012-06-06 23:15:27 +0100

    simplify api

--- pikos/api.py
+++ pikos/api.py
@@ -3,16 +3,33 @@ import sys
 from pikos.monitor import Monitor
 from pikos.recorders.text_stream_recorder import TextStreamRecorder
 from pikos.monitors.function_monitor import FunctionMonitor
+from pikos.monitors.line_monitor import LineMonitor
+from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
 
-def baserecorder(filter_):
-    """ Factory function that returns a default recorder.
+def baserecorder(filter_=None):
+    """ Factory function that returns a basic recorder.
     """
     return TextStreamRecorder(sys.stdout, filter_=filter_, auto_flush=True)
 
-def log_functions(filter_):
-    """ Factory function that returns a default function logger.
+def monitor_functions(filter_=None):
+    """ Factory function that returns a basic function monitor.
     """
-    return FunctionMonitor(baserecorder(filter_=filter_))
+    return Monitor(FunctionMonitor(baserecorder(filter_=filter_)))
+
+def monitor_lines(filter_=None):
+    """ Factory function that returns a basic line monitor.
+    """
+    return Monitor(LineMonitor(baserecorder(filter_=filter_)))
+
+def memory_on_functions(filter_=None):
+    """ Factory function that returns a basic line monitor.
+    """
+    return Monitor(FunctionMemoryMonitor(baserecorder(filter_=filter_)))
+
+def memory_on_lines(filter_=None):
+    """ Factory function that returns a basic line monitor.
+    """
+    return Monitor(LineMemoryMonitor(baserecorder(filter_=filter_)))
 
 #: Easy to find placeholder for the Monitor decorator class.
 monitor = Monitor
