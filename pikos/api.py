4aa2aaeb1cc8d78b1fc14069b0f4a75c5c7c4a8a
itziakos <ioannist@enthought.com>
2012-06-08 04:05:40 +0100

    Update api, move imports inside functions to avoid poluting the namespace

--- pikos/api.py
+++ pikos/api.py
@@ -1,34 +1,35 @@
-import sys
 
 from pikos.monitor import Monitor
-from pikos.recorders.text_stream_recorder import TextStreamRecorder
-from pikos.monitors.function_monitor import FunctionMonitor
-from pikos.monitors.line_monitor import LineMonitor
-from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
 
 def baserecorder(filter_=None):
     """ Factory function that returns a basic recorder.
     """
+    import sys
+    from pikos.recorders.text_stream_recorder import TextStreamRecorder
     return TextStreamRecorder(sys.stdout, filter_=filter_, auto_flush=True)
 
 def monitor_functions(filter_=None):
     """ Factory function that returns a basic function monitor.
     """
+    from pikos.monitors.function_monitor import FunctionMonitor
     return Monitor(FunctionMonitor(baserecorder(filter_=filter_)))
 
 def monitor_lines(filter_=None):
     """ Factory function that returns a basic line monitor.
     """
+    from pikos.monitors.line_monitor import LineMonitor
     return Monitor(LineMonitor(baserecorder(filter_=filter_)))
 
 def memory_on_functions(filter_=None):
     """ Factory function that returns a basic line monitor.
     """
+    from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
     return Monitor(FunctionMemoryMonitor(baserecorder(filter_=filter_)))
 
 def memory_on_lines(filter_=None):
     """ Factory function that returns a basic line monitor.
     """
+    from pikos.monitors.line_memory_monitor import LineMemoryMonitor
     return Monitor(LineMemoryMonitor(baserecorder(filter_=filter_)))
 
 #: Easy to find placeholder for the Monitor decorator class.
