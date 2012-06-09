a399c8acaeba4048ff5949ef155a006d48f0d54e
Simon Jagoe <sjagoe@enthought.com>
2012-06-09 09:52:41 +0800

    Added copy of cProfile for extension to use zmq for real-time recording

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 #  file: setup.py
 #  License: LICENSE.TXT
@@ -6,13 +6,26 @@
 #  Copyright (c) 2011, Enthought, Inc.
 #  All rights reserved.
 #------------------------------------------------------------------------------
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 
-setup(name='pikos',
+extensions = [
+    Extension(
+        'pikos._internal._lsprof_rt',
+        sources=[
+            'pikos/_internal/_lsprof_rt.c',
+            'pikos/_internal/rotatingtree.c',
+            ],
+        ),
+    ]
+
+setup(
+    name='pikos',
     version='0.1a',
     author='Enthought, Inc',
     author_email='ioannist@enthought.com',
     description='Enthought profiling tools',
     requires=['psutil'],
     install_requires=['distribute'],
-    packages=find_packages())
+    packages=find_packages(),
+    ext_modules=extensions,
+    )
