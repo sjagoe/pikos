863b4701ea896776958357dd4e281aa29fb39e45
Simon Jagoe <sjagoe@enthought.com>
2012-06-11 06:15:51 +0100

    Added silly cprofile-rt example

---
+++ examples/cprofile_rt.py
@@ -0,0 +1,14 @@
+# Run this with ``python -m pikos._internal.cProfile_rt examples/cprofile_rt.py``
+
+import time
+import random
+import numpy as np
+
+def main():
+    for i in xrange(10):
+        arr = np.zeros((1000, 1000))
+        time.sleep(random.random())
+
+
+if __name__ == '__main__':
+    main()
