0906f5ebefa02b022b418abc4ada4c04eee4af48
Ioannis Tziakos <ioannist@enthought.com>
2012-11-02 19:56:54 +0000

    Rename TraceFunctions to TraceFunctionManager and fix issue where the previous tracefunction is not recovered.

---
+++ pikos/tests/test_trace_function_manager.py
@@ -0,0 +1,63 @@
+import sys
+import unittest
+
+from pikos._internal.trace_function_manager import TraceFunctionManager
+
+class MockNativeMonitor():
+
+    def __init__(self):
+        self._profile = TraceFunctionManager()
+
+    def __enter__(self):
+        self._profile.replace(self.event)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._profile.recover()
+
+    def event(self, frame, event, arg):
+        return self.event
+
+class TestTraceFunctionsManager(unittest.TestCase):
+
+    def setUp(self):
+        self.old = sys.gettrace()
+        def foo():
+            pass
+        def bar(frame, event, arg):
+            pass
+        self.foo = foo
+        self.bar = bar
+        self.monitor = MockNativeMonitor()
+
+    def test_preserving_previous_function(self):
+
+        sys.settrace(self.bar)
+        with self.monitor:
+            self.foo()
+        self.assertIs(sys.gettrace(), self.bar)
+
+    def test_error_when_set_multiple(self):
+        self.monitor._profile.replace(self.bar)
+        self.assertIs(sys.gettrace(), self.bar)
+        with self.assertRaises(AssertionError),\
+             self.assertRaises(RuntimeError):
+            self.monitor._profile.replace(self.bar)
+            self.assertIs(sys.gettrace(), self.bar)
+        self.monitor._profile.recover()
+
+        self.monitor._profile.replace(self.bar)
+        self.assertIs(sys.gettrace(), self.bar)
+        with self.assertRaises(RuntimeError):
+            self.monitor._profile.replace(None)
+            self.assertIs(sys.gettrace(), self.bar)
+        self.monitor._profile.recover()
+
+    def test_error_when_unset(self):
+        with self.assertRaises(RuntimeError):
+            self.monitor._profile.recover()
+
+    def tearDown(self):
+        sys.settrace(self.old)
+
+if __name__ == '__main__':
+    unittest.main()
