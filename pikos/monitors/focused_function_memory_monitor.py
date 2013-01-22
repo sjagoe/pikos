f6387daa097afbdf2f37b7a258d139b35ad5b243
Ioannis Tziakos <ioannist@enthought.com>
2013-01-22 22:50:52 +0000

    add a focused function mixin and provide stub classes for focused function monitors

--- pikos/monitors/focused_function_memory_monitor.py
+++ pikos/monitors/focused_function_memory_monitor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 #  Package: Pikos toolkit
-#  File: monitors/focused_function_monitor.py
+#  File: monitors/focused_function_memory_monitor.py
 #  License: LICENSE.TXT
 #
 #  Copyright (c) 2012, Enthought, Inc.
@@ -9,15 +9,17 @@
 #------------------------------------------------------------------------------
 from __future__ import absolute_import
 
-from pikos.monitors.function_monitor import FunctionMonitor
+from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
 from pikos.monitors.focused_function_mixin import FocusedFunctionMixin
 
 
-class FocusedFunctionMonitor(FocusedFunctionMixin, FunctionMonitor):
-    """ Record python function events.
+class FocusedFunctionMemoryMonitor(FunctionMemoryMonitor,
+                                   FocusedFunctionMixin):
+    """ Record process memory on python function events.
 
     The class hooks on the setprofile function to receive function events and
-    record them if they take place inside the provided functions.
+    record while inside the provided functions the current process memory
+    when they happen.
 
     Public
     ------
@@ -28,24 +30,28 @@ class FocusedFunctionMonitor(FocusedFunctionMixin, FunctionMonitor):
     Private
     -------
     _recorder : object
-        A recorder object that implementes the
+        A recorder object that implements the
         :class:`~pikos.recorder.AbstractRecorder` interface.
 
     _profiler : object
         An instance of the
-        :class:`~pikos._internal.profiler_functions.ProfilerFunctions`
-        utility class that is used to set and unset the setprofile function
-        as required by the monitor.
+        :class:`~pikos._internal.profiler_functions.ProfilerFunctions` utility
+        class that is used to set and unset the setprofile function as
+        required
+        by the monitor.
 
     _index : int
-        The current zero based record index. Each function event will increase
-        the index by one.
+        The current zero based record index. Each function event will
+        increase the index by one.
 
     _call_tracker : object
         An instance of the :class:`~pikos._internal.keep_track` utility class
         to keep track of recursive calls to the monitor's :meth:`__enter__`
-         and
-        :meth:`__exit__` methods.
+        and :meth:`__exit__` methods.
+
+    _process : object
+       An instance of :class:`psutil.Process` for the current process,
+       used to get memory information in a platform independent way.
 
     _code_trackers : dictionary
         A dictionary of KeepTrack instances associated with the code object
---
+++ pikos/monitors/focused_function_mixin.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+#  Package: Pikos toolkit
+#  File: monitors/focused_function_mixin.py
+#  License: LICENSE.TXT
+#
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from pikos._internal.function_set import FunctionSet
+from pikos._internal.keep_track import KeepTrack
+
+
+class FocusedFunctionMixin(object):
+    """ Mixing class to support recording python function events in a
+    `focused` way.
+
+    The method is used along a function event based monitor. It mainly
+    overrides the on_function_event method to only record events when the
+    interpreter is working inside the predefined functions.
+
+    Public
+    ------
+    functions : FunctionSet
+        A set of function or method objects inside which recording will
+        take place.
+
+    Private
+    -------
+    _code_trackers : dictionary
+        A dictionary of KeepTrack instances associated with the code object
+        of each function in `functions`. It is used to keep track and check
+        that we are inside the execution of one these functions when we
+        record data.
+
+    """
+
+    def __init__(self, *arguments, **keywords):
+        """ Initialize the monitoring class.
+
+        Parameters
+        ----------
+        *arguments : list
+            The list of arguments required by the base monitor. They will
+            be passed on the super class of the mixing
+
+        **keywords : dict
+            Dictionary of keyword arguments. The `functions` keyword if
+            defined should be a list of function or method objects inside
+            which recording will take place.
+
+        """
+        functions = keywords.pop('functions', ())
+        super(FocusedFunctionMixin, self).__init__(*arguments, **keywords)
+        self.functions = FunctionSet(functions)
+        self._code_trackers = {}
+
+    def on_function_event(self, frame, event, arg):
+        """ Record the current function event only when we are inside one
+        of the provided functions.
+
+        """
+        code = frame.f_code
+        event_method = super(FocusedFunctionMixin, self).on_function_event
+        if code in self.functions:
+            tracker = self._code_trackers.setdefault(code, KeepTrack())
+            if event == 'call':
+                tracker('ping')
+            else:
+                tracker('pong')
+            event_method(frame, event, arg)
+            if not tracker:
+                del self._code_trackers[code]
+        elif any(self._code_trackers.viewvalues()):
+            event_method(frame, event, arg)
