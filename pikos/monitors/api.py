9ef7b748a6753d59071d5f019f8a75c69d78fac2
Ioannis Tziakos <ioannist@enthought.com>
2013-01-22 22:51:52 +0000

    update api files

--- pikos/monitors/api.py
+++ pikos/monitors/api.py
@@ -2,3 +2,10 @@ from pikos.monitors.function_monitor import FunctionMonitor
 from pikos.monitors.line_monitor import LineMonitor
 from pikos.monitors.function_memory_monitor import FunctionMemoryMonitor
 from pikos.monitors.line_memory_monitor import LineMemoryMonitor
+
+from pikos.monitors.focused_function_monitor import FocusedFunctionMonitor
+from pikos.monitors.focused_line_monitor import FocusedLineMonitor
+from pikos.monitors.focused_function_memory_monitor import \
+    FocusedFunctionMemoryMonitor
+from pikos.monitors.focused_line_memory_monitor import \
+    FocusedLineMemoryMonitor
