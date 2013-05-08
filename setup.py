383e6b56395a0f34ec7de9ec52acbd5330dbec7c
Simon Jagoe <sjagoe@enthought.com>
2013-05-08 12:09:19 +0100

    Write a version file in the package

--- setup.py
+++ setup.py
@@ -19,12 +19,44 @@ real_time_lsprof = Feature(
             'pikos._internal._lsprof_rt',
             sources=['pikos/_internal/_lsprof_rt.c',
                      'pikos/_internal/rotatingtree.c'],
-            libraries=['zmq'])]
-    )
+            libraries=['zmq'],
+        ),
+    ]
+)
+
+
+VERSION = '0.1.0dev1'
+
+
+def write_version_py(filename=None):
+    if filename is None:
+        filename = os.path.join(os.path.dirname(__file__),
+                                'pikos', 'version.py')
+    ver = """\
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+#  Package: Pikos toolkit
+#  File: version.py
+#  License: LICENSE.TXT
+#
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+version = '%s'
+"""
+    fh = open(filename, 'wb')
+    try:
+        fh.write(ver % VERSION)
+    finally:
+        fh.close()
+
+
+write_version_py()
+
 
 setup(
     name='pikos',
-    version='0.1a',
+    version=VERSION,
     author='Enthought Inc',
     author_email='info@enthought.com',
     description='Enthought monitoring and profiling tools',
